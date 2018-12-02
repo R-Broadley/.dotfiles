87c49c528e129f754f11b961f3a3f4252d66aa13
Rob Broadley <git@rbroadley.co.uk>
2018-12-02 13:37:29 +0000

    fixup! Added initial zsh config

--- zsh/config/antigen.zsh
+++
@@ -1,8 +0,0 @@
-# Syntax highlighting bundle.
-antigen bundle zsh-users/zsh-syntax-highlighting
-
-# Load the theme.
-antigen theme bhilburn/powerlevel9k powerlevel9k
-
-# Tell Antigen that you're done.
-antigen apply
