76a27520b00c8b2c9b1a27c8686737315b5e84ba
Rob Broadley <git@rbroadley.co.uk>
2018-12-02 19:34:09 +0000

    fixup! Added initial zsh config

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-FONT="DejaVu Sans Mono Nerd Font Complete Mono"
+FONT="DejaVuSansMono Nerd Font Mono"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # Mac OSX
-    ln -sv ~/dotfiles/fonts/nerd-fonts ~/Library/Fonts/nerd-fonts
+    cp -r ~/dotfiles/fonts/nerd-fonts ~/Library/Fonts
     osascript -e "tell application \"Terminal\" to set the font name of window 1 to \"$FONT\""
 elif [[ "$OSTYPE" == "linux-gnu" ]]; then
     ln -sv ~/dotfiles/fonts/nerd-fonts ~/.fonts/nerd-fonts
